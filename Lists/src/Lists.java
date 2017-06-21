8dabf86f61e526a6f1e9f996aa193f735761ef66
Margarita_Nekrashevich <margarita_nekrashevich@EPUSSANW0020.local>
2017-06-20 23:40:47 -0700

    Jun20 - develop - 2nd commit

--- Lists/src/Lists.java
+++ Lists/src/Lists.java
@@ -5,6 +5,7 @@ public class Lists {
 //test comment
 //new comment
 //develop comment
+//develop comment - 2nd
  public static void main(String[] args) throws IOException {
   List<String> list = new ArrayList<String>();
   Scanner in = new Scanner(new File("C:\\1\\File.txt"));
